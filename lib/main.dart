1c4881427c08f45fb70e38f7e946e6658180ed22
londonreturns <subedioscar.47@gmail.com>
2023-04-19 06:42:30 +0545

    auto update

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {  runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.indigo.shade100
-    ),
-    body: Center(
-      child: Text(
-        'hello world',
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.black,
-              fontFamily: 'new',
-      ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('press'),
-      backgroundColor: Colors.indigo.shade700,
-      onPressed: (){},
-    ),
-  ),
+  home: Home(),
 ));
 }
 
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.indigo.shade100
+      ),
+      body: Center(
+        child: Text(
+          'hello world',
+          style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.black,
+            fontFamily: 'new',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('press'),
+        backgroundColor: Colors.indigo.shade700,
+        onPressed: (){},
+      ),
+    );
+  }
+}
